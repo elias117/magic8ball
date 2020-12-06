0842fe7f32493209a46cdffa90591b1394883554
Elias Zoghaib <elias.zoghaib@dal.ca>
2020-12-06 17:15:00 -0400

    I added the question and answer functionality

--- magic8ball.js
+++ magic8ball.js
@@ -20,3 +20,19 @@ let possible_answers = [
     "Yes – definitely.",
     "You may rely on it.",
 ];
+
+function get_random_message() {
+    let random_index = Math.floor(Math.random() * possible_answers.length);
+    let random_message = possible_answers[random_index];
+    return random_message;
+}
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+readline.question(`What's your question?\n\n`, (question) => {
+    console.log(`The answer to ${question} is`);
+    console.log(get_random_message());
+    readline.close();
+});
